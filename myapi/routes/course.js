bbfea008bf0d71b6616044fffbd5c471d31b28bd
vatue <thevatu1@gmail.com>
2021-02-21 22:54:25 +0200

    myapi muokkausta example6

---
+++ myapi/routes/course.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const course = require('../models/course_model');
+
+router.get('/:id?',
+ function(request, response) {
+  if (request.params.id) {
+    course.getById(request.params.id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  } else {
+    course.get(function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  }
+});
+
+
+router.post('/', 
+function(request, response) {
+    course.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(request.body);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+    course.delete(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.put('/:id', 
+function(request, response) {
+    course.update(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
